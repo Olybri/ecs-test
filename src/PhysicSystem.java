bcd4c6dd9c8f78b9a51bdb7ad28dc3d9a13be1cf
Loris Witschard <loris.wit@gmail.com>
2016-11-18 14:10:57 +0100

    Add lateral collisions

--- src/PhysicSystem.java
+++ src/PhysicSystem.java
@@ -23,10 +23,22 @@ public class PhysicSystem extends BaseSystem
             speed.add(Vector.scale(gravity, elapsedTime));
             position.add(Vector.scale(speed, elapsedTime));
             
-            if(position.y >= 445)
+            if(position.y >= 440)
             {
-                position.y = 445;
+                position.y = 440;
                 speed.y *= -0.8;
+                speed.x *= 0.995;
+            }
+            if(position.x <= 0)
+            {
+                position.x = 0;
+                speed.x *= -1;
+            }
+    
+            if(position.x >= 790)
+            {
+                position.x = 790;
+                speed.x *= -1;
             }
             
             entity.setComponent(Component.Position, position);
